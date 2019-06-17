db701257f865b52a298aafb14b2aeb033ca568b4
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-06-17 12:13:36 +0430

    feat: some minor changes in 2d-array-ds.

--- src/2d-array-ds/index.js
+++ src/2d-array-ds/index.js
@@ -2,7 +2,17 @@ const computeMaxHourGlasses = (arr) => {
   let max = Number.NEGATIVE_INFINITY;
   for (let i = 0 ; i < arr.length - 2  ; i++) {
     for (let j = 0 ; j < arr[i].length - 2 ; j++) {
-      const temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+      let temp = 0;
+      for (let k = 0; k < 3 ; k++) {
+        for (let l = 0 ; l < 3; l++) {
+          if ( (k !== 1 && l !== 0) || (k !== 1 && l !== 2) ) {
+            temp += arr[i + k][j + l];
+          }
+          if ( k === 1  && l === 1) {
+            temp += arr[i + k][j + l];
+          }
+        }
+      }
       if (max < temp) {
         max = temp;
       }
@@ -29,5 +39,5 @@ const arr2 = [
   [-3, -6, 0, -8, -6, -7]
 ]
 
-console.log(computeMaxHourGlasses(arr));
-console.log(computeMaxHourGlasses(arr2));
+console.log(computeMaxHourGlasses(arr)); // 19
+console.log(computeMaxHourGlasses(arr2)); // -19
