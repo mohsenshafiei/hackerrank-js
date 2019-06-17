a68e785e4e5256b5aa3cf6d0701c33da46cbdad2
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-06-17 12:57:06 +0430

    feat: add trempoline to the super-digit challenge.

--- src/super-digit/index.js
+++ src/super-digit/index.js
@@ -1,11 +1,22 @@
 function sum(total, v) { return total + parseInt(v); }
 
-function superDigit(num) {
+function trempoline(fn) {
+  return function trempoline(...args) {
+    let result = fn(...args);
+    while (typeof result === "function") {
+      result = result();
+    }
+    return result;
+  }
+};
+
+const superDigit = trempoline(function f(num) {
   const arr = num.split('');
   if (arr.length === 1) {
     return arr[0];
   } else {
-    return superDigit(arr.reduce(sum, 0).toString());
+    return f(arr.reduce(sum, 0).toString());
   }
-}
+});
+
 console.log(superDigit((2332).toString()));
