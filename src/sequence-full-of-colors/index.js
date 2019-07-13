3a8bfa34d1ddc3a8af057b67c8ba033b8eeaf03e
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-07-13 12:48:29 +0430

    feat: add sequence full of colors problem.

---
+++ src/sequence-full-of-colors/index.js
@@ -0,0 +1,59 @@
+const validateEquality = (colors) => {
+  return ((colors.green === colors.red) && (colors.yellow === colors.blue));
+}
+
+const validatePrefixDistance = (colors) => {
+  if (Math.abs(colors.red - colors.green) > 1) {
+    return false;
+  }
+  if (Math.abs(colors.blue - colors.yellow) > 1) {
+    return false;
+  }
+  return true;
+}
+
+const validate = (str) => {
+  let ok = true;
+  const final = str.split('').reduce((result, character) => {
+    switch (character) {
+      case 'R':
+        result = {
+          ...result,
+          red: result.red + 1,
+        }
+        break;
+      case 'G':
+        result = {
+          ...result,
+          green: result.green + 1,
+        }
+        break;
+      case 'B':
+        result = {
+          ...result,
+          blue: result.blue + 1,
+        }
+        break;
+      case 'Y':
+        result = {
+          ...result,
+          yellow: result.yellow + 1,
+        }
+        break;
+      default:
+    }
+    if (ok === true) {
+      ok = validatePrefixDistance(result);
+    }
+    return result;
+  }, {
+    red: 0,
+    blue: 0,
+    green: 0,
+    yellow: 0,
+  });
+  console.log(final);
+  return ok && validateEquality(final);
+}
+
+console.log(validate('RYRB'));
